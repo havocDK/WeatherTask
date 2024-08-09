f6c3c982121d2bcb6881bc0da266299f077c2725
Dimitar kolev <shadowovercliff@gmail.com>
2024-08-09 08:54:41 +0300

    Initial Commit

---
+++ WeatherTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherTask
+//
+//  Created by Dimitar Kolev on 9.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherTask/WeatherTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTaskApp.swift
+//  WeatherTask
+//
+//  Created by Dimitar Kolev on 9.08.24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
